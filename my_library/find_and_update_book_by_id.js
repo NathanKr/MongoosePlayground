6a1beecab2e2be058004f8b8501d1b92303f52ae
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-12-11 10:21:27 +0200

    fix update

--- my_library/find_and_update_book_by_id.js
+++ my_library/find_and_update_book_by_id.js
@@ -1,18 +1,21 @@
 const Book = require("./models/book").Book;
-require('./my_db');
-
-let updatedInfo = {};
+require("./my_db");
 
 const _id = process.argv[2];
-const name = process.argv[3];
-const pages = Number(process.argv[4]);
-const isnew = (process.argv[5] == 'true'); 
+const name = process.argv[3] ? process.argv[3] : "book name updated";
 
-// ------- todo change updatedInfo
+// --- only name is upated
+
+const filter = { _id };
+const update = { name };
 
 // --- this will be called once the connection is established successfully
-Book.findByIdAndUpdate(_id,updatedInfo, function (err, book) {
-  if (err) return console.error(err);
-  console.log(book ? 'book is updated successfully' : `book _id : ${_id} was not found`);
-})
-
+Book.findOneAndUpdate(
+  filter,
+  { $set: update },
+  { useFindAndModify: false }, // --- this removes warning
+  function(err, book) {
+    if (err) return console.error(err);
+    console.log(book ? book : `book _id : ${_id} was not found`);
+  }
+);
